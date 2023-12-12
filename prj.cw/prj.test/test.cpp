f5c1837f8da34935ac6de5085c3654da5c7ff7e0
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2023-12-12 22:29:23 +0300

    Update test.cpp

--- prj.cw/prj.test/test.cpp
+++ prj.cw/prj.test/test.cpp
@@ -2,59 +2,56 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    if (argc < 6) {
-        std::cerr << "Usage: " << argv[0] << " <type> <numPoints> <blurCoefficient> <distributionMin> <distributionMax>" << std::endl;
+    if (argc < 8) {
+        std::cerr << "Usage: " << argv[0] << " <type> <numPoints> <blurCoefficient> <startX> <startY> <endX> <endY>" << std::endl;
         return 1;
     }
+
+    Data data;
 
     std::string type = argv[1];
     int numPoints = std::stoi(argv[2]);
     double blurCoefficient = std::stod(argv[3]);
-    double distributionMin = std::stod(argv[4]);
-    double distributionMax = std::stod(argv[5]);
+    double startX = std::stod(argv[4]);
+    double startY = std::stod(argv[5]);
+    double endX = std::stod(argv[6]);
+    double endY = std::stod(argv[7]);
 
-    if (type == "points") {
-        generatePointsWithNoise(numPoints, blurCoefficient, distributionMin, distributionMax);
-//        std::vector<CartesianCoordinates> readPoints = readPointsFromFile("noisy_points.txt");
-//        for (const auto& coord : readPoints) {
-//            std::cout << "x: " << coord.x << ", y: " << coord.y << std::endl;
-//        }
-    }
+    Data::Point startPoint{ startX, startY };
+    Data::Point endPoint{ endX, endY };
 
-    else if (type == "segments") {
-        generateSegments(numPoints, blurCoefficient, distributionMin, distributionMax);
-//        std::vector<CartesianForSegmentsCoordinates> readVectors = readSegmentsFromFile("noisy_segments.txt");
+
+    if (type == "segment_from_point") {
+       data.generateSegmentsFromPoint(numPoints, blurCoefficient, startPoint, endPoint);
+//        std::vector<Data::SegmentsCoordinates> readVectors = data.readSegmentsFromFile("noisy_segments_from_point.txt");
 //        for (const auto& coordinates : readVectors) {
 //            std::cout << "x: " << coordinates.x << ", y: " << coordinates.y
 //                      << ", x1: " << coordinates.x1 << ", y1: " << coordinates.y1 << std::endl;
 //        }
     }
 
-    else if (type == "parallel_segments") {
-        generateParallelSegments(numPoints, blurCoefficient, distributionMin, distributionMax);
-//        std::vector<CartesianForSegmentsCoordinates> readParallelSegments = readSegmentsFromFile("noisy_parallel_segments.txt");
-//        for (const auto& coordinates : readParallelSegments) {
-//            std::cout << "x: " << coordinates.x << ", y: " << coordinates.y
-//                      << ", x1: " << coordinates.x1 << ", y1: " << coordinates.y1 << std::endl;
-//        }
-    }
-
-    else if (type == "segments_from_point") {
-        if (argc < 8) {
-            std::cerr << "Usage for segments_from_point: " << argv[0] << "segments_from_point <numPoints> <blurCoefficient> <startPoint.x> <startPoint.y>" << std::endl;
-            return 1;
+    else if (type == "segments") {
+        data.generateSegments(numPoints, blurCoefficient, startPoint, endPoint);
+                std::vector<Data::SegmentsCoordinates> readVectors = data.readSegmentsFromFile("segments.txt");
+        for (const auto& coordinates : readVectors) {
+            std::cout << "x: " << coordinates.x << ", y: " << coordinates.y
+                      << ", x1: " << coordinates.x1 << ", y1: " << coordinates.y1 << std::endl;
         }
 
-        CartesianCoordinates startPoint;
-        startPoint.x = std::stod(argv[6]);
-        startPoint.y = std::stod(argv[7]);
+    }
 
-        generateSegmentsFromPoint(numPoints, blurCoefficient,distributionMin, distributionMax, startPoint);
-        std::vector<CartesianForSegmentsCoordinates> readSegmentsFromPoint = readSegmentsFromFile("noisy_segments_from_point.txt");
-//        for (const auto& coordinates : readSegmentsFromPoint ) {
-//            std::cout << "x: " << coordinates.x << ", y: " << coordinates.y
-//                      << ", x1: " << coordinates.x1 << ", y1: " << coordinates.y1 << std::endl;
-//        }
+    else if (type == "repeat_segments") {
+        data.generateRepeatSegments(numPoints, blurCoefficient, startPoint, endPoint);
+    }
+
+    else if (type == "segment_with_dif_len") {
+        if (argc < 10) {
+            std::cerr << "Usage for segments_from_point: " << argv[0] << " <type> <numPoints> <blurCoefficient> <startX> <startY> <endX> <endY> <minLength> <maxLength>" << std::endl;
+            return 1;
+        }
+        double minLength = std::stod(argv[8]);
+        double maxLength = std::stod(argv[9]);
+        data.generateSegmentsWithDidLen(numPoints, blurCoefficient, startPoint, endPoint, minLength, maxLength);
     }
 
     else {

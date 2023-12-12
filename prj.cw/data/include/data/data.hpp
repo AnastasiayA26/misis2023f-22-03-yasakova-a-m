a9381fd957cc4a7900fd69dd5f2cfc79894a5a82
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2023-12-12 22:28:22 +0300

    Update data.hpp

--- prj.cw/data/include/data/data.hpp
+++ prj.cw/data/include/data/data.hpp
@@ -1,38 +1,110 @@
 #pragma once
-#ifndef DENVIZ_DATA_H
-#define DENVIZ_DATA_H
+#ifndef DATA_HPP
+#define DATA_HPP
 
-#include <vector>
+#include <iostream>
 #include <fstream>
-#include <string>
+#include <vector>
+#include <random>
+#include <iomanip>
 
-struct CartesianCoordinates {
-    double x;
-    double y;
+/**
+ * @brief Class Data for generating and processing coordinate data.
+ */
+class Data {
+public:
+    /**
+     * @brief Structure representing a point in two-dimensional space.
+     */
+    struct Point {
+        double x; /**< X-coordinate */
+        double y; /**< Y-coordinate */
+    };
+
+    /**
+     * @brief Structure representing the coordinates of linear segments in two-dimensional space.
+     */
+    struct SegmentsCoordinates {
+        double x, y, x1, y1; /**< Initial and final coordinates of the linear segment */
+    };
+
+    /**
+     * @brief Constructor of the Data class.
+     */
+    Data();
+
+    /**
+     * @brief Writes the coordinates of linear segments to a file.
+     * @param coordinates Vector of Point representing linear segments.
+     * @param filename Output file name.
+     */
+    void writeSegmentsToFile(const std::vector<Point>& coordinates, const std::string& filename);
+
+    /**
+     * @brief Writes individual points to a file.
+     * @param coordinates Vector of Point representing individual points.
+     * @param filename Output file name.
+     */
+    void writePointsToFile(const std::vector<Point>& coordinates, const std::string& filename);
+
+    /**
+     * @brief Generates linear segments starting from a specified point.
+     * @param numSegments Number of generated linear segments.
+     * @param noiseStd Standard deviation of Gaussian noise for coordinates.
+     * @param start Starting point of the segments.
+     * @param end Ending point of the segments.
+     */
+    void generateSegmentsFromPoint(int numSegments, double noiseStd, const Point& start, const Point& end);
+
+    /**
+     * @brief Generates linear segments between two points.
+     * @param numPoints Number of generated points.
+     * @param noiseStd Standard deviation of Gaussian noise for coordinates.
+     * @param start Starting point of the segments.
+     * @param end Ending point of the segments.
+     */
+    void generateSegments(int numPoints, double noiseStd, const Point& start, const Point& end);
+
+    /**
+     * @brief Generates linear segments with specified length constraints.
+     * @param numSegments Number of generated linear segments.
+     * @param noiseStd Standard deviation of Gaussian noise for coordinates.
+     * @param start Starting point of the segments.
+     * @param end Ending point of the segments.
+     * @param minLength Minimum length of the segments.
+     * @param maxLength Maximum length of the segments.
+     */
+    void generateSegmentsWithDidLen(int numSegments, double noiseStd, const Point& start, const Point& end, double minLength, double maxLength);
+
+    /**
+     * @brief Generates repeating linear segments.
+     * @param numSegments Number of generated linear segments.
+     * @param noiseStd Standard deviation of Gaussian noise for coordinates.
+     * @param start Starting point of the segments.
+     * @param end Ending point of the segments.
+     */
+    void generateRepeatSegments(int numSegments, double noiseStd, const Point& start, const Point& end);
+
+    /**
+     * @brief Reads points from a file.
+     * @param filename Input file name.
+     * @return Vector of SegmentsCoordinates representing points.
+     */
+    std::vector<SegmentsCoordinates> readPointsFromFile(const char* filename);
+
+    /**
+     * @brief Reads linear segments from a file.
+     * @param filename Input file name.
+     * @return Vector of SegmentsCoordinates representing linear segments.
+     */
+    std::vector<SegmentsCoordinates> readSegmentsFromFile(const char* filename);
+
+private:
+    std::default_random_engine generator; /**< Random number generator */
+    std::normal_distribution<double> distribution; /**< Normal distribution for Gaussian noise */
+    std::uniform_real_distribution<double> lengthDistribution; /**< Uniform distribution for segment length */
 };
-struct CartesianForSegmentsCoordinates {
-    double x, y, x1, y1;
-};
 
-CartesianCoordinates addGaussianNoise(const CartesianCoordinates& original, double stddev);
+#endif // DATA_HPP
 
-CartesianCoordinates generateRandomPoint(double distributionMin, double distributionMax);
-
-void writePointsToFile(const std::vector<CartesianCoordinates>& coordinates, const std::string& filename);
-
-void writeSegmentsToFile(const std::vector<CartesianCoordinates>& coordinates, const std::string& filename);
-
-void generatePointsWithNoise(int numPoints, double blurCoefficient, double distributionMin, double distributionMax);
-
-void generateParallelSegments(int numSegments, double stddev, double distributionMin, double distributionMax);
-
-void generateSegmentsFromPoint(int numSegments, double stddev, double distributionMin, double distributionMax, const CartesianCoordinates& startPoint);
-
-void generateSegments(int numSegments, double stddev, double distributionMin, double distributionMax);
-
-std::vector<CartesianCoordinates> readPointsFromFile(const char* filename);
-
-std::vector<CartesianForSegmentsCoordinates> readSegmentsFromFile(const char* filename);
-
-#endif //DENVIZ_DATA_H
 

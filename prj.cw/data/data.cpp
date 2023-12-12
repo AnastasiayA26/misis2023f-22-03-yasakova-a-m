e0b35d27fcd80656d354139c8a61b25334290ae2
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2023-12-12 22:27:35 +0300

    Update data.cpp

--- prj.cw/data/data.cpp
+++ prj.cw/data/data.cpp
@@ -4,44 +4,10 @@
 #include <fstream>
 #include <iomanip>
 
-CartesianCoordinates addGaussianNoise(const CartesianCoordinates& original, double stddev) {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::normal_distribution<double> distribution(0.0, stddev);
-
-    CartesianCoordinates noisy;
-    noisy.x = original.x + distribution(gen);
-    noisy.y = original.y + distribution(gen);
-    return noisy;
+Data::Data() : distribution(0.0, 1.0), lengthDistribution(0.0, 5.0) {
 }
 
-CartesianCoordinates generateRandomPoint(double distributionMin, double distributionMax) {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    std::uniform_real_distribution<double> distribution(distributionMin, distributionMax);
-
-    CartesianCoordinates point;
-    point.x = distribution(gen);
-    point.y = distribution(gen);
-    return point;
-}
-
-void writePointsToFile(const std::vector<CartesianCoordinates>& coordinates, const std::string& filename) {
-    std::ofstream outFile(filename);
-    if (!outFile.is_open()) {
-        std::cerr << "Unable to open file: " << filename << std::endl;
-        return;
-    }
-
-    outFile << std::fixed << std::setprecision(2);
-
-    for (const auto& point : coordinates) {
-        outFile << point.x << ' ' << point.y << '\n';
-    }
-    outFile.close();
-}
-
-void writeSegmentsToFile(const std::vector<CartesianCoordinates>& coordinates, const std::string& filename) {
+void Data::writeSegmentsToFile(const std::vector<Point>& coordinates, const std::string& filename) {
     std::ofstream outFile(filename);
     if (!outFile.is_open()) {
         std::cerr << "Unable to open file: " << filename << std::endl;
@@ -58,135 +24,119 @@ void writeSegmentsToFile(const std::vector<CartesianCoordinates>& coordinates, c
     outFile.close();
 }
 
-
-void generatePointsWithNoise(int numPoints, double blurCoefficient, double distributionMin, double distributionMax) {
-    std::vector<CartesianCoordinates> noisyPoints;
-    std::vector<CartesianCoordinates> cleanPoints;
-    std::vector<CartesianCoordinates> differences;
-
-    for (int i = 0; i < numPoints; ++i) {
-        CartesianCoordinates cleanPoint = generateRandomPoint(distributionMin, distributionMax);
-        CartesianCoordinates noisyPoint = addGaussianNoise(cleanPoint, blurCoefficient);
-
-        cleanPoints.push_back(cleanPoint);
-        noisyPoints.push_back(noisyPoint);
-
-        CartesianCoordinates diff;
-        diff.x = noisyPoint.x - cleanPoint.x;
-        diff.y = noisyPoint.y - cleanPoint.y;
-        differences.push_back(diff);
+void Data::writePointsToFile(const std::vector<Point>& coordinates, const std::string& filename) {
+    std::ofstream outFile(filename);
+    if (!outFile.is_open()) {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+        return;
     }
 
-    // Write clean points, noisy points, and differences to files
-    writePointsToFile(cleanPoints, "clean_points.txt");
-    writePointsToFile(noisyPoints, "noisy_points.txt");
-    writePointsToFile(differences, "difference_points.txt");
+    outFile << std::fixed << std::setprecision(2);
 
+    for (const auto& point : coordinates) {
+        outFile << point.x << ' ' << point.y << '\n';
+    }
+    outFile.close();
 }
 
+// из одной точки рисуется несколько
+void Data::generateSegmentsFromPoint(int numSegments, double noiseStd, const Point& start, const Point& end) {
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(0.0, noiseStd);
 
-void generateParallelSegments(int numSegments, double stddev, double distributionMin, double distributionMax) {
-    std::vector<CartesianCoordinates> cleanSegments;
-    std::vector<CartesianCoordinates> noisySegments;
-    std::vector<CartesianCoordinates> differences;
+    std::vector<Point> segments;
 
     for (int i = 0; i < numSegments; ++i) {
-        CartesianCoordinates cleanStartPoint = generateRandomPoint(distributionMin, distributionMax);
-        CartesianCoordinates cleanEndPoint = generateRandomPoint(distributionMin, distributionMax);
+        int segmentLength = distribution(generator) + 10;
 
-        CartesianCoordinates noisyStartPoint = addGaussianNoise(cleanStartPoint, stddev);
-        CartesianCoordinates noisyEndPoint = addGaussianNoise(cleanEndPoint, stddev);
+        double deltaX = (end.x - start.x) / (segmentLength - 1);
+        double deltaY = (end.y - start.y) / (segmentLength - 1);
 
-        cleanSegments.push_back(cleanStartPoint);
-        cleanSegments.push_back(cleanEndPoint);
+        Point segmentStart = start;
+        Point segmentEnd;
 
-        noisySegments.push_back(noisyStartPoint);
-        noisySegments.push_back(noisyEndPoint);
-
-        CartesianCoordinates diffStartPoint;
-        diffStartPoint.x = noisyStartPoint.x - cleanStartPoint.x;
-        diffStartPoint.y = noisyStartPoint.y - cleanStartPoint.y;
-        differences.push_back(diffStartPoint);
-
-        CartesianCoordinates diffEndPoint;
-        diffEndPoint.x = noisyEndPoint.x - cleanEndPoint.x;
-        diffEndPoint.y = noisyEndPoint.y - cleanEndPoint.y;
-        differences.push_back(diffEndPoint);
+        segmentEnd.x = start.x + (segmentLength - 1) * deltaX + distribution(generator);;
+        segmentEnd.y = start.y + (segmentLength - 1) * deltaY + distribution(generator);
+        segments.push_back(segmentStart);
+        segments.push_back(segmentEnd);
     }
-
-    writeSegmentsToFile(cleanSegments, "clean_segments.txt");
-    writeSegmentsToFile(noisySegments, "noisy_segments.txt");
-    writeSegmentsToFile(differences, "difference_segments.txt");
-
+    writeSegmentsToFile(segments, "segment_from_point.txt");
 }
 
-void generateSegmentsFromPoint(int numSegments, double stddev, double distributionMin, double distributionMax, const CartesianCoordinates& startPoint) {
-    std::vector<CartesianCoordinates> cleanSegments;
-    std::vector<CartesianCoordinates> noisySegments;
-    std::vector<CartesianCoordinates> differences;
+void Data::generateSegments(int numPoints, double noiseStd, const Point& start, const Point& end) {
+    std::vector<Point> noisyPoints;
+    double deltaX = (end.x - start.x) / (numPoints - 1);
+    double deltaY = (end.y - start.y) / (numPoints - 1);
 
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<double> distribution(0.0, noiseStd);
+//    Point p;
+//    noisyPoints.push_back(p);
+    for (int j = 0; j < numPoints; ++j) {
+        Point p;
+        p.x = start.x + j * deltaX;
+        p.y = start.y + j * deltaY;
+        noisyPoints.push_back(p);
+        p.x = start.x + j * deltaX + distribution(gen);
+        p.y = start.y + j * deltaY + distribution(gen);
+        noisyPoints.push_back(p);
+    }
+    writeSegmentsToFile(noisyPoints, "segments.txt");
+}
+
+// из одной точки но с разной длиной
+void Data::generateSegmentsWithDidLen(int numSegments, double noiseStd, const Point& start, const Point& end, double minLength, double maxLength) {
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(0.0, noiseStd);
+    std::uniform_real_distribution<double> lengthDistribution(minLength, maxLength);
+
+    std::vector<Point> segments;
+
+    double mainSegmentLength = std::hypot(end.x - start.x, end.y - start.y) / 3;
+    double angle = M_PI / 3.0;  // Угол поворота 60
+
+
+    for (int j = 0; j < numSegments; ++j) {
+        Point nestedSegmentStart;
+        nestedSegmentStart.x = start.x + j * (mainSegmentLength / numSegments) * cos(angle);
+        nestedSegmentStart.y = start.y + j * (mainSegmentLength / numSegments) * sin(angle);
+
+        Point nestedSegmentEnd;
+        double randomLength = lengthDistribution(generator);
+        nestedSegmentEnd.x = nestedSegmentStart.x + randomLength * cos(angle) + distribution(generator);
+        nestedSegmentEnd.y = nestedSegmentStart.y + randomLength * sin(angle) + distribution(generator);
+
+        segments.push_back(nestedSegmentStart);
+        segments.push_back(nestedSegmentEnd);
+    }
+
+    writeSegmentsToFile(segments, "segments_from_point_with_dif_len.txt");
+}
+void Data::generateRepeatSegments(int numSegments, double noiseStd, const Point& start, const Point& end) {
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(0.0, noiseStd);
+
+    std::vector<Point> resultPoints;
     for (int i = 0; i < numSegments; ++i) {
-        CartesianCoordinates cleanEndPoint = generateRandomPoint(distributionMin, distributionMax);
+        Point noisyStart;
+        noisyStart.x = start.x + distribution(generator);
+        noisyStart.y = start.y + distribution(generator);
 
-        CartesianCoordinates noisyEndPoint = addGaussianNoise(cleanEndPoint, stddev);
+        Point noisyEnd;
+        noisyEnd.x = end.x + distribution(generator);
+        noisyEnd.y = end.y + distribution(generator);
 
-        cleanSegments.push_back(startPoint);
-        cleanSegments.push_back(cleanEndPoint);
-
-        noisySegments.push_back(startPoint);
-        noisySegments.push_back(noisyEndPoint);
-
-        CartesianCoordinates diffEndPoint;
-        diffEndPoint.x = noisyEndPoint.x - cleanEndPoint.x;
-        diffEndPoint.y = noisyEndPoint.y - cleanEndPoint.y;
-        differences.push_back(diffEndPoint);
+        resultPoints.push_back(noisyStart);
+        resultPoints.push_back(noisyEnd);
     }
 
-    writeSegmentsToFile(cleanSegments, "clean_segments_from_point.txt");
-    writeSegmentsToFile(noisySegments, "noisy_segments_from_point.txt");
-    writeSegmentsToFile(differences, "difference_segments_from_point.txt");
-
-}
-
-void generateSegments(int numSegments, double stddev, double distributionMin, double distributionMax) {
-    std::vector<CartesianCoordinates> cleanSegments;
-    std::vector<CartesianCoordinates> noisySegments;
-    std::vector<CartesianCoordinates> differences;
-
-    for (int i = 0; i < numSegments; ++i) {
-        CartesianCoordinates cleanStartPoint = generateRandomPoint(distributionMin, distributionMax);
-        CartesianCoordinates cleanEndPoint = generateRandomPoint(distributionMin, distributionMax);
-
-        CartesianCoordinates noisyStartPoint = addGaussianNoise(cleanStartPoint, stddev);
-        CartesianCoordinates noisyEndPoint = addGaussianNoise(cleanEndPoint, stddev);
-
-        // Save clean and noisy segments
-        cleanSegments.push_back(cleanStartPoint);
-        cleanSegments.push_back(cleanEndPoint);
-
-        noisySegments.push_back(noisyStartPoint);
-        noisySegments.push_back(noisyEndPoint);
-
-        // Calculate and save the difference
-        CartesianCoordinates diffStartPoint;
-        diffStartPoint.x = noisyStartPoint.x - cleanStartPoint.x;
-        diffStartPoint.y = noisyStartPoint.y - cleanStartPoint.y;
-        differences.push_back(diffStartPoint);
-
-        CartesianCoordinates diffEndPoint;
-        diffEndPoint.x = noisyEndPoint.x - cleanEndPoint.x;
-        diffEndPoint.y = noisyEndPoint.y - cleanEndPoint.y;
-        differences.push_back(diffEndPoint);
-    }
-
-    writeSegmentsToFile(cleanSegments, "clean_segments.txt");
-    writeSegmentsToFile(noisySegments, "noisy_segments.txt");
-    writeSegmentsToFile(differences, "difference_segments.txt");
+    writeSegmentsToFile(resultPoints, "repeat_segments.txt");
 }
 
 
-std::vector<CartesianCoordinates> readPointsFromFile(const char* filename) {
-    std::vector<CartesianCoordinates> coordinates;
+std::vector<Data::SegmentsCoordinates> Data::readPointsFromFile(const char* filename) {
+    std::vector<SegmentsCoordinates> coordinates;
     double x, y;
 
     std::ifstream inputFile(filename);
@@ -197,7 +147,7 @@ std::vector<CartesianCoordinates> readPointsFromFile(const char* filename) {
     }
 
     while (inputFile >> x >> y) {
-        CartesianCoordinates coord{ x, y };
+        SegmentsCoordinates coord{ x, y };
         coordinates.push_back(coord);
     }
 
@@ -205,8 +155,8 @@ std::vector<CartesianCoordinates> readPointsFromFile(const char* filename) {
     return coordinates;
 }
 
-std::vector<CartesianForSegmentsCoordinates> readSegmentsFromFile(const char* filename) {
-    std::vector<CartesianForSegmentsCoordinates> vectors;
+std::vector<Data::SegmentsCoordinates> Data::readSegmentsFromFile(const char* filename) {
+    std::vector<SegmentsCoordinates> vectors;
     double x, y, z, w;
 
     std::ifstream inputFile(filename);
@@ -217,11 +167,10 @@ std::vector<CartesianForSegmentsCoordinates> readSegmentsFromFile(const char* fi
     }
 
     while (inputFile >> x >> y >> z >> w) {
-        CartesianForSegmentsCoordinates coordinates{ x, y, z, w };
+        SegmentsCoordinates coordinates{ x, y, z, w };
         vectors.push_back(coordinates);
     }
 
     inputFile.close();
     return vectors;
 }
-

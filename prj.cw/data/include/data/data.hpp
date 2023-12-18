e4b7344267ab3d0c0acf647355ecc2ee784a2144
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2023-12-18 11:24:33 +0300

    Update data.hpp

--- prj.cw/data/include/data/data.hpp
+++ prj.cw/data/include/data/data.hpp
@@ -9,101 +9,66 @@
 #include <iomanip>
 
 /**
- * @brief Class Data for generating and processing coordinate data.
+ * @brief Class for generating spherical coordinates and working with them.
  */
-class Data {
+class SphericalCoordinatesGenerator {
 public:
     /**
-     * @brief Structure representing a point in two-dimensional space.
+     * @brief Default constructor.
      */
-    struct Point {
-        double x; /**< X-coordinate */
-        double y; /**< Y-coordinate */
-    };
+    SphericalCoordinatesGenerator();
 
     /**
-     * @brief Structure representing the coordinates of linear segments in two-dimensional space.
+     * @brief Writes points to a file.
+     * @param filename File name to write the points.
+     * @param points Vector of vectors representing spherical coordinates of points.
      */
-    struct SegmentsCoordinates {
-        double x, y, x1, y1; /**< Initial and final coordinates of the linear segment */
-    };
-
-    /**
-     * @brief Constructor of the Data class.
-     */
-    Data();
-
-    /**
-     * @brief Writes the coordinates of linear segments to a file.
-     * @param coordinates Vector of Point representing linear segments.
-     * @param filename Output file name.
-     */
-    void writeSegmentsToFile(const std::vector<Point>& coordinates, const std::string& filename);
-
-    /**
-     * @brief Writes individual points to a file.
-     * @param coordinates Vector of Point representing individual points.
-     * @param filename Output file name.
-     */
-    void writePointsToFile(const std::vector<Point>& coordinates, const std::string& filename);
-
-    /**
-     * @brief Generates linear segments starting from a specified point.
-     * @param numSegments Number of generated linear segments.
-     * @param noiseStd Standard deviation of Gaussian noise for coordinates.
-     * @param start Starting point of the segments.
-     * @param end Ending point of the segments.
-     */
-    void generateSegmentsFromPoint(int numSegments, double noiseStd, const Point& start, const Point& end);
-
-    /**
-     * @brief Generates linear segments between two points.
-     * @param numPoints Number of generated points.
-     * @param noiseStd Standard deviation of Gaussian noise for coordinates.
-     * @param start Starting point of the segments.
-     * @param end Ending point of the segments.
-     */
-    void generateSegments(int numPoints, double noiseStd, const Point& start, const Point& end);
-
-    /**
-     * @brief Generates linear segments with specified length constraints.
-     * @param numSegments Number of generated linear segments.
-     * @param noiseStd Standard deviation of Gaussian noise for coordinates.
-     * @param start Starting point of the segments.
-     * @param end Ending point of the segments.
-     * @param minLength Minimum length of the segments.
-     * @param maxLength Maximum length of the segments.
-     */
-    void generateSegmentsWithDidLen(int numSegments, double noiseStd, const Point& start, const Point& end, double minLength, double maxLength);
-
-    /**
-     * @brief Generates repeating linear segments.
-     * @param numSegments Number of generated linear segments.
-     * @param noiseStd Standard deviation of Gaussian noise for coordinates.
-     * @param start Starting point of the segments.
-     * @param end Ending point of the segments.
-     */
-    void generateRepeatSegments(int numSegments, double noiseStd, const Point& start, const Point& end);
+    void writePointsToFile(const std::string &filename, const std::vector<std::vector<double>> &points) const;
 
     /**
      * @brief Reads points from a file.
-     * @param filename Input file name.
-     * @return Vector of SegmentsCoordinates representing points.
+     * @param filename File name to read the points.
+     * @return Vector of vectors representing spherical coordinates of points.
      */
-    std::vector<SegmentsCoordinates> readPointsFromFile(const char* filename);
+    std::vector<std::vector<double>> readPointsFromFile(const std::string &filename) const;
 
     /**
-     * @brief Reads linear segments from a file.
-     * @param filename Input file name.
-     * @return Vector of SegmentsCoordinates representing linear segments.
+     * @brief Generates points on a spherical arc.
+     * @param numPoints Number of points to generate.
+     * @param phi_start Starting phi angle in radians.
+     * @param theta_start Starting theta angle in radians.
+     * @param phi_end Ending phi angle in radians.
+     * @param theta_end Ending theta angle in radians.
+     * @return Vector of vectors representing the generated spherical coordinates of points.
      */
-    std::vector<SegmentsCoordinates> readSegmentsFromFile(const char* filename);
+    std::vector<std::vector<double>> generatePointsOnArc(int numPoints, double phi_start, double theta_start, double phi_end, double theta_end) const;
 
-private:
-    std::default_random_engine generator; /**< Random number generator */
-    std::normal_distribution<double> distribution; /**< Normal distribution for Gaussian noise */
-    std::uniform_real_distribution<double> lengthDistribution; /**< Uniform distribution for segment length */
+    /**
+     * @brief Adds noise to the spherical coordinates of points on a sphere.
+     * @param points Vector of vectors representing spherical coordinates of points.
+     * @param noiseLevel The level of noise to add.
+     * @return Vector of vectors representing spherical coordinates of noisy points.
+     */
+    std::vector<std::vector<double>> addCoordinateNoiseOnSphere(const std::vector<std::vector<double>> &points, double noiseLevel) const;
+
+    /**
+  * @brief Generates points on the surface of a sphere from a single point, drawing multiple arcs within a specified range.
+  *
+  * @param numArcs The number of arcs to draw.
+  * @param numPointsPerArc The number of points generated on each arc.
+  * @param centerPhi Azimuthal angle of the central point.
+  * @param centerTheta Polar angle of the central point.
+  * @param phi_start Starting limit of the azimuthal angle for generating points on the arc.
+  * @param theta_start Starting limit of the polar angle for generating points on the arc.
+  * @param phi_end Ending limit of the azimuthal angle for generating points on the arc.
+  * @param theta_end Ending limit of the polar angle for generating points on the arc.
+  *
+  * @return Returns a vector of vectors representing the coordinates of the generated points on the surface of the sphere.
+  *         The first index of the outer vector corresponds to the arcs, and the second index of the inner vector corresponds to the coordinates of points (theta, phi).
+  */
+    std::vector<std::vector<double>> generatePointsFromCenter(int numArcs, int numPointsPerArc, double centerPhi, double centerTheta, double phi_start, double theta_start, double phi_end, double theta_end) const;
 };
+
 
 #endif // DATA_HPP
 

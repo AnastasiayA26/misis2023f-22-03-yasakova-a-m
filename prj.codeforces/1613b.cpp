d161acaff53632fe11307223a695d3a88568afb1
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2023-12-21 21:28:41 +0300

    Update 1613b.cpp

--- prj.codeforces/1613b.cpp
+++ prj.codeforces/1613b.cpp
@@ -1,26 +1,27 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 #include <algorithm>
 
-
-int main(){
+int main() {
     int t;
-    std::cin>>t;
+    std::cin >> t;
 
-    while(t--){
+    while (t--) {
         int n;
-        std::cin>>n;
-        int arr[n];
+        std::cin >> n;
+        std::vector<int> arr(n);
 
-        for(int i=0; i<n; i++){
-            std::cin>>arr[i];
+        for (int i = 0; i < n; i++) {
+            std::cin >> arr[i];
         }
 
-        std::sort(arr, arr + n);
+        std::sort(arr.begin(), arr.end());
 
-        for(int i=0; i<n/2; i++){
-            std::cout<<arr[n-i-1]<<" "<<arr[0]<<std::endl;
+        for (int i = 0; i < n / 2; i++) {
+            std::cout << arr[n - i - 1] << " " << arr[0] << std::endl;
         }
     }
 
     return 0;
 }
+

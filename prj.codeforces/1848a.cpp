8f4cba8c75292b27549c87e93d9d10024e6726ef
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2023-12-21 21:31:27 +0300

    Update 1848a.cpp

--- prj.codeforces/1848a.cpp
+++ prj.codeforces/1848a.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 #include <vector>
 
-int main()
-{
-    int t,n,m,k,x1,y1,b;
+int main() {
+    int t, n, m, k, x1, y1, b;
     std::cin >> t;
-    while(t--)
-    {
-        bool co=0;
+    while (t--) {
+        bool co = false;
         std::cin >> n >> m >> k;
-        int x[k] , y[k] ;
+        std::vector<int> x(k);
+        std::vector<int> y(k);
+
         std::cin >> x1 >> y1;
-        b=(x1+y1)%2;
-        for(int i=0; i<k;i++)
-        {
+        b = (x1 + y1) % 2;
+
+        for (int i = 0; i < k; i++) {
             std::cin >> x[i] >> y[i];
         }
-        for(int i=0; i<k;i++)
-            if((x[i]+y[i])%2==b)
-            {
-                co=1;
+
+        for (int i = 0; i < k; i++) {
+            if ((x[i] + y[i]) % 2 == b) {
+                co = true;
                 break;
             }
-        if(co>0)  std::cout << "NO" << std::endl;
-        else       std::cout << "YES" << std::endl;
+        }
+
+        if (co) {
+            std::cout << "NO" << std::endl;
+        } else {
+            std::cout << "YES" << std::endl;
+        }
     }
+
+    return 0;
 }
+
  
